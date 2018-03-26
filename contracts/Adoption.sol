50dd3bbf041ed91fc74722538c4e89df73ac8aad
cohki0305 <cohki0305@gmail.com>
2018-03-26 09:28:29 +0900

    add contract, adoption

---
+++ contracts/Adoption.sol
@@ -0,0 +1,18 @@
+// The pragma command means "additional information that only the compiler cares about"
+pragma solidity ^0.4.17;
+
+contract Adoption {
+  address[16] public adopters;
+
+  function adopt(uint petId) public returns (uint) {
+    require(petId >= 0 && petId <= 15);
+
+    adopters[petId] = msg.sender;
+
+    return petId;
+  }
+
+  function getAdopters() public view returns (address[16]) {
+    return adopters;
+  }  
+}
